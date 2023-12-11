ecb3e90aa2e4fd2aa400812a56f33a80332acb11
Christian Fernandez <christian14@gmail.com>
2023-12-11 18:34:20 -0300

    Creando componentes / Algunos estilos (96)

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import NuevoPresupuesto from "./NuevoPresupuesto"
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Planifiador de gastos</h1>
+      <NuevoPresupuesto />
+    </header>
+  )
+}
+
+export default Header
