4ac3447fdf8aa80ab655895b598fa7a574d93a43
Christian Fernandez <christian14@gmail.com>
2023-12-12 09:52:13 -0300

    Creando el formulario de Gastos / incorporando algo al CSS (106)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,6 +4,15 @@ import CerrarModal from '../img/cerrar.svg'
 
 const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
+  const CATEGORIAS = [
+    'ahorro',
+    'comida',
+    'casa',
+    'gastos Varios',
+    'oscio',
+    'salud',
+    'suscripciones']
+
   const ocultarModal = () => {
     setAnimarModal(false)
 
@@ -25,7 +34,46 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
       </div>
 
       <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
+
         <legend>Nuevo Gasto</legend>
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            id='nombre'
+            type="text"
+            placeholder='Ingresa el nombre del gasto'
+          />
+
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id='cantidad'
+            type="number"
+            placeholder='Ingresa la cantidad: Ej. 300'
+          />
+
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+
+          <select id="categoria">
+            <option value="">- seleccione -</option>
+            {CATEGORIAS.map(categoria => (
+              <option
+                key={categoria}
+                value={categoria}
+              >{categoria}</option>
+            ))}
+
+          </select>
+        </div>
+
+        <input type="submit" value="Añadir Gasto" />
+
       </form>
 
     </div>
