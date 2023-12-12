c5b0d1a8c660fc15f20f1688876b13344712c096
Christian Fernandez <christian14@gmail.com>
2023-12-12 17:22:11 -0300

    Agregando efecto Swipe al componente Gasto / estructura basica de una dependencia de React (116-117)

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Proyecto de Crontrol de Gastos
 => Css NO usaremos Frameworks (Pero debemo instalar Normalize.css, para este proyecto)
 => Incorporamos fuentes desde google fonts (lato)
 => El index.css que esta en el proyecto, queda de manera global en todo el proyecto
+=> Instalacion de react-swipeable-list (npm i react-swipeable-list), para darle algunas animaciones
+   a las ediciones.
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
 
 import { formatarFecha } from "../helpers";
 import { formatearCantidad } from "../helpers";
@@ -24,28 +32,52 @@ const diccionarioIconos = {
 const Gasto = ({ gasto }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
 
+// Efecto Swipe
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => console.info('Editando')}>
+        Editar
+      </SwipeAction>
+    </LeadingActions>
+  );
+  
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction
+        destructive={true}
+        onClick={() => console.info('Eliminado')}
+      >
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  );
+
 
   return (
-    <div className='gasto sombra'>
-
-      <div className='contenido-gasto'>
-        <img
-          src={diccionarioIconos[categoria]}
-          alt={`Imagen del gasto ${categoria}`}
-        />
-        <div className="descripcion-gasto">
-          <p className="categoria">{categoria}</p>
-          <p className="nombre-gasto">{nombre}</p>
-          <p className='fecha-gasto'>
-            Agredado: {' '}
-            <span>{formatarFecha(fecha)}</span>
-          </p>
-
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
+        <div className='gasto sombra'>
+          <div className='contenido-gasto'>
+            <img
+              src={diccionarioIconos[categoria]}
+              alt={`Imagen del gasto ${categoria}`}
+            />
+            <div className="descripcion-gasto">
+              <p className="categoria">{categoria}</p>
+              <p className="nombre-gasto">{nombre}</p>
+              <p className='fecha-gasto'>
+                Agredado: {' '}
+                <span>{formatarFecha(fecha)}</span>
+              </p>
+            </div>
+          </div>
+          <p className="cantidad-gasto">{formatearCantidad(cantidad)}</p>
         </div>
-      </div>
-
-      <p className="cantidad-gasto">{formatearCantidad(cantidad)}</p>
-    </div>
+      </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
