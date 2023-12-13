95677f1ff27fca4010e9a644e6493f34955bfc02
Christian Fernandez <christian14@gmail.com>
2023-12-13 13:19:09 -0300

    Cambiando la apariencia de Presupuesto diponible si ya fue sobrepasado (132)

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -36,7 +36,8 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
       <div>
         <CircularProgressbar
         styles={buildStyles({
-          pathColor:'#3b82f6',
+          pathColor: porcentaje > 100 ? '#dc2626': '#3b82f6',
+          textColor: porcentaje > 100 ? '#dc2626': '#3b82f6',
           trailColor: '#f5f5f5'
         })}
           value={porcentaje}
@@ -50,7 +51,7 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
           <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
 
-        <p>
+        <p className={disponible < 0 ? 'negativo' : ''}>
           <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
 
